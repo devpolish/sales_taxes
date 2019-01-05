f66ae4f9ff83e823b10641dfcb567dbea53ef717
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-01-05 09:14:55 -0500

    add csv helper

---
+++ lib/sales_taxes/helpers/csv.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+# This helpers aims to read/write CSV files.
+module CSVHelper
+  HEADERS = %w[quantity product price].freeze
+
+  def self.read_file(pathname)
+    CSV.read(pathname)
+  rescue Errno::ENOENT => exception
+    "#{exception.class} File not found"
+  end
+
+  def self.write_file(pathname, data)
+    CSV.open(pathname, 'wb') do |csv|
+      # Add extra column without unfrezee array
+      csv << HEADERS.dup.concat(['sales taxes'])
+      data.each { |row| csv << row }
+    end
+  rescue Errno::ENOENT => exception
+    "#{exception.class} File not found"
+  end
+end
